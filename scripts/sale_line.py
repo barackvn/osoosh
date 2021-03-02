865d370d6c316328b2d406df8a80ceae51f81418
dontk1 <komolafetosin@gmail.com>
2021-03-02 23:52:59 +0100

    sale line

--- scripts/sale_line.py
+++ scripts/sale_line.py
@@ -27,75 +27,84 @@ offset = 0 + done
 print('Offset:', offset)
 
 order_lines = models_v_9.execute_kw(db_v_9, uid_v_9, password_v_9,
-    'sale.order.line', 'search_read',[[]],{'offset': offset, 'limit': size})
+'sale.order.line', 'search_read',[[]],{'offset': offset, 'limit': size})
 
-    for line in order_lines:
-        line['database_id_v9'] = line['id']
-        line['order_partner_id'] = order['partner_id']
-        line['currency_id'] = order['currency_id']
-        line['company_id'] = order['company_id']
-        line['order_id'] = order_id
+for i, line in enumerate(order_lines):
+    line['database_id_v9'] = line['id']
+    if line['order_partner_id']:
+        partner_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
+            'res.partner', 'search_read',[[('database_id_v9','=',line['order_partner_id'][0])]],{'limit': size})
+        if partner_id:
+            line['order_partner_id'] = partner_id[0]['id']
+        else:
+            line['order_partner_id'] = False
+    line['currency_id'] = 9
+   
+    if line['company_id']:
+        company_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
+            'res.company', 'search',[[('name','=',line['company_id'][1])]],{'limit': size})
+        line['company_id'] = company_id[0]
+    
+    if line['order_id']:
+        order_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
+            'sale.order', 'search',[[('database_id_v9','=',line['order_id'][0])]],{'limit': 1})
+        if order_id:
+            line['order_id'] = order_id[0]
+        else:
+            continue
 
-        if line['order_id']:
-            order_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
-                'sale.order', 'search',[[('database_id_v9','=',line['order_id'][0])]],{'limit': 1})
-            if order_id:
-                line['order_id'] = order_id[0]
-            else:
-                continue
+    if line['product_uom']:
+        line['product_uom'] = line['product_uom'][0]
+    
+    if line['product_id']:
+        product_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
+        'product.product', 'search_read',[[('name','=',line['product_id'][1])]],{'limit': size})
+        if product_id:
+            line['product_id'] = product_id[0]['id']
+        else:
+            line['product_id'] = False
+    # if line['product_tmpl_id']:
+    #     product_tmpl_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
+    #     'product.template', 'search_read',[[('database_id_v9','=',line['product_tmpl_id'][0])]],{'limit': size})
+    #     if product_tmpl_id:
+    #         line['product_tmpl_id'] = product_tmpl_id[0]['id']
+    #     else:
+    #         line['product_tmpl_id'] = False
+    
+    # salesman_id
+    if line['salesman_id']:
+        try:
+            user = models_v_9.execute_kw(db_v_9, uid_v_9, password_v_9,
+                'res.users', 'search_read',[[('id','=',line['salesman_id'][0])]],{'limit': size})[0]
+            line['salesman_id'] = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
+            'res.users', 'search',[[('login','=',user['login'])]],{'limit': size})[0]
+        except:
+            line['salesman_id'] = False
+    
+    
+    del line['event_ticket_id']
+    del line['attendee_ids']
+    del line['invoice_lines']
+    del line['procurement_ids']
+    del line['reservation_ids']
+    del line['event_id']
+    del line['option_line_id']
+    del line['product_pack_count']
+    del line['event_type_id']
+    del line['qty_delivered_updateable']
+    del line['discounted_price']
+    del line['is_low_forecast']
+    del line['is_low_stock']
+    del line['is_stock_reservable']
+    del line['x_bseprice']
+    del line['product_tmpl_id']
+    del line['property_ids']
+    # del line['is_low_forecast']
+    # del line['is_low_forecast']
+    # del line['is_low_forecast']
+    
 
-        if line['product_uom']:
-            line['product_uom'] = line['product_uom'][0]
-        
-        if line['product_id']:
-            product_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
-            'product.product', 'search_read',[[('name','=',line['product_id'][1])]],{'limit': size})
-            if product_id:
-                line['product_id'] = product_id[0]['id']
-            else:
-                line['product_id'] = False
-        # if line['product_tmpl_id']:
-        #     product_tmpl_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
-        #     'product.template', 'search_read',[[('database_id_v9','=',line['product_tmpl_id'][0])]],{'limit': size})
-        #     if product_tmpl_id:
-        #         line['product_tmpl_id'] = product_tmpl_id[0]['id']
-        #     else:
-        #         line['product_tmpl_id'] = False
-        
-        # salesman_id
-        if line['salesman_id']:
-            try:
-                user = models_v_9.execute_kw(db_v_9, uid_v_9, password_v_9,
-                    'res.users', 'search_read',[[('id','=',line['salesman_id'][0])]],{'limit': size})[0]
-                line['salesman_id'] = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14,
-                'res.users', 'search',[[('login','=',user['login'])]],{'limit': size})[0]
-            except:
-                line['salesman_id'] = False
-        
-        
-        del line['event_ticket_id']
-        del line['attendee_ids']
-        del line['invoice_lines']
-        del line['procurement_ids']
-        del line['reservation_ids']
-        del line['event_id']
-        del line['option_line_id']
-        del line['product_pack_count']
-        del line['event_type_id']
-        del line['qty_delivered_updateable']
-        del line['discounted_price']
-        del line['is_low_forecast']
-        del line['is_low_stock']
-        del line['is_stock_reservable']
-        del line['x_bseprice']
-        del line['product_tmpl_id']
-        del line['property_ids']
-        # del line['is_low_forecast']
-        # del line['is_low_forecast']
-        # del line['is_low_forecast']
-        
-
-        # print(line)
-        line_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14, 'sale.order.line', 'create', [line])
+    # print(line)
+    line_id = models_v_14.execute_kw(db_v_14, uid_v_14, password_v_14, 'sale.order.line', 'create', [line])
 
     print("Processed [%s] %s of %s [%s] %s"%(line_id, i, size, 100 * i/size, '%'))
